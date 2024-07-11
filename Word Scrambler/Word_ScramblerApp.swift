a44234ce92c22905d3095fa620a3cb873cce8980
V Yashwanth Raj <99544533+YashwanthRaj@users.noreply.github.com>
2024-07-11 15:21:27 -0400

    Add files via upload

---
+++ Word Scrambler/Word_ScramblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScramblerApp.swift
+//  Word Scrambler
+//
+//  Created by Yashwanth Raj Varadharajan on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScramblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
